ff0ff7186ac1dcc2063477d3a2199828961911c6
Guruprasad3n <gp76652@gmail.com>
2024-03-19 22:44:30 +0530

    added design

--- FigmaDesign/src/components/Layout.jsx
+++ FigmaDesign/src/components/Layout.jsx
@@ -5,18 +5,28 @@ import search from "../../public/assets/search.png";
 import disc from "../../public/assets/disc.png";
 import logout from "../../public/assets/logout.png";
 import SearchBar from "./SearchBar";
+import { useNavigate } from "react-router-dom";
 function Layout({ children }) {
+  const navigate = useNavigate();
+  const handleCLick = () => {
+    navigate("/");
+  };
   return (
     <>
       <div className="main">
         <div className="layout">
           <div className="sidebar">
             <div>
-              <img src={mainLogo} alt="Logo" />
-              <img src={home} alt="Home" />
-              <img src={search} alt="Search" />
-              <img src={disc} alt="Disc" />
-              <img src={logout} alt="Logout" />
+              <img
+                src={mainLogo}
+                onClick={handleCLick}
+                alt="Logo"
+                style={{ cursor: "pointer" }}
+              />
+              <img src={home} alt="Home" style={{ cursor: "pointer" }} />
+              <img src={search} alt="Search" style={{ cursor: "pointer" }} />
+              <img src={disc} alt="Disc" style={{ cursor: "pointer" }} />
+              <img src={logout} alt="Logout" style={{ cursor: "pointer" }} />
             </div>
           </div>
           <div className="content">
